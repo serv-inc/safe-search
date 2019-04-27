058dc9b02b1146647a35c9a0575ab958674bd55d
serv-inc <serv-inc@users.noreply.github.com>
2019-04-27 20:05:19 +0200

    test: lacked ddg loading and searching

--- test/funtest.py
+++ test/funtest.py
@@ -12,4 +12,14 @@ webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
 
 profile = webdriver.FirefoxProfile()
 profile.add_extension(extension="./addon/")
-driver = webdriver.Firefox(firefox_profile=profile) 
+browser = webdriver.Firefox(firefox_profile=profile)
+
+browser.get("https://duckduckgo.com")
+assert "duckduckgo" in browser.title.lower()
+(browser
+ .find_element_by_id("search_form_input_homepage")
+ .send_keys("porn" + webdriver.common.keys.Keys.RETURN))
+
+# click to disable temporarily, then wait, then see if available
+browser.find_element_by_class_name("js-safe-search-temp").click()
+#driver.implicitly_wait(10) # seconds
