cb273a0a9c1238cdf5ab8406aafda48393ad5edd
serv-inc <serv-inc@users.noreply.github.com>
2019-04-28 07:13:32 +0200

    test: lacked ddg safe search validation

--- test/funtest.py
+++ test/funtest.py
@@ -10,16 +10,20 @@ webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
     "proxyType":"MANUAL"
  }
 
+# setup addon
 profile = webdriver.FirefoxProfile()
 profile.add_extension(extension="./addon/")
 browser = webdriver.Firefox(firefox_profile=profile)
 
+# load ddg
 browser.get("https://duckduckgo.com")
 assert "duckduckgo" in browser.title.lower()
+# search porn
 (browser
  .find_element_by_id("search_form_input_homepage")
  .send_keys("porn" + webdriver.common.keys.Keys.RETURN))
-
 # click to disable temporarily, then wait, then see if available
 browser.find_element_by_class_name("js-safe-search-temp").click()
-#driver.implicitly_wait(10) # seconds
+# assert that browser.find_element_by_class_name("js-safe-search-temp") still exists
+browser.find_element_by_class_name("js-safe-search-temp")
+
