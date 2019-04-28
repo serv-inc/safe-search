af859fdea97357adb923a72086ca4d77df56f432
serv-inc <serv-inc@users.noreply.github.com>
2019-04-28 23:32:58 +0200

    test: lacked python unittest for ddg on ff

--- test/funtest.py
+++ test/funtest.py
@@ -1,4 +1,9 @@
+import os
+import unittest
 from selenium import webdriver
+
+os.environ['PATH'] = os.environ['PATH'] + ":."
+
 
 # local testing, remove before CI
 PROXY='127.0.0.1:8080'
@@ -8,22 +13,33 @@ webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
     "sslProxy":PROXY,
     "noProxy":[],
     "proxyType":"MANUAL"
- }
+}
 
-# setup addon
-profile = webdriver.FirefoxProfile()
-profile.add_extension(extension="./addon/")
-browser = webdriver.Firefox(firefox_profile=profile)
+class DuckDuckGoTestCase(unittest.TestCase):
 
-# load ddg
-browser.get("https://duckduckgo.com")
-assert "duckduckgo" in browser.title.lower()
-# search porn
-(browser
- .find_element_by_id("search_form_input_homepage")
- .send_keys("porn" + webdriver.common.keys.Keys.RETURN))
-# click to disable temporarily, then wait, then see if available
-browser.find_element_by_class_name("js-safe-search-temp").click()
-# assert that browser.find_element_by_class_name("js-safe-search-temp") still exists
-browser.find_element_by_class_name("js-safe-search-temp")
+    def setUp(self):
+        profile = webdriver.FirefoxProfile()
+        profile.add_extension(extension="../addon/")
+        self.browser = webdriver.Firefox(firefox_profile=profile)
 
+    def testDuckLoads(self):
+        self.browser.get("https://duckduckgo.com")
+        self.assertIn("duckduckgo", self.browser.title.lower())
+
+    def testDuckSafe(self):
+        self.browser.get("https://duckduckgo.com")
+        (self.browser
+         .find_element_by_id("search_form_input_homepage")
+         .send_keys("porn" + webdriver.common.keys.Keys.RETURN))
+        # click to disable temporarily, then wait, then see if available
+        self.browser.implicitly_wait(10)
+        self.browser.find_element_by_class_name("js-safe-search-temp").click()
+        # assert that browser.find_element_by_class_name("js-safe-search-temp") still exists, else throws exception
+        self.browser.find_element_by_class_name("js-safe-search-temp")
+
+    def tearDown(self):
+        self.browser.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
