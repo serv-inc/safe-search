7d7f2960acd96105640b1acd9b259be6e9695809
serv-inc <serv-inc@users.noreply.github.com>
2019-04-30 07:15:29 +0200

    bug: failed on DDG after their update

---
+++ test/funtest.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python2
+import os
+import unittest
+from selenium import webdriver
+
+DIR = os.path.dirname(__file__)
+
+os.environ['PATH'] = os.environ['PATH'] + ":" + DIR
+
+
+# local testing, remove before CI
+PROXY='127.0.0.1:8080'
+webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
+    "httpProxy":PROXY,
+    "ftpProxy":PROXY,
+    "sslProxy":PROXY,
+    "noProxy":[],
+    "proxyType":"MANUAL"
+}
+
+class DuckTest(unittest.TestCase):
+    def setUp(self):
+        profile = webdriver.FirefoxProfile()
+        profile.add_extension(
+            extension=os.path.join(DIR, "..", "addon"))
+        self.browser = webdriver.Firefox(firefox_profile=profile)
+
+    def test_duck_safe(self):
+        self.browser.get("https://duckduckgo.com")
+        (self.browser
+         .find_element_by_id("search_form_input_homepage")
+         .send_keys("porn" + webdriver.common.keys.Keys.RETURN))
+        # click to disable temporarily, then wait, then see if available
+        self.browser.implicitly_wait(10)
+        self.browser.find_element_by_class_name("js-safe-search-temp").click()
+        # assert that browser.find_element_by_class_name("js-safe-search-temp") still exists, else throws exception
+        self.browser.find_element_by_class_name("js-safe-search-temp")
+
+    def tearDown(self):
+        self.browser.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
