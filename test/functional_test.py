9d585337c6c02ef71d1a78235b13597a1bf7fcd4
serv-inc <serv-inc@users.noreply.github.com>
2019-05-02 12:21:06 +0200

    test: lacked Chromium ddg test

--- test/functional_test.py
+++ test/functional_test.py
@@ -8,24 +8,19 @@ DIR = os.path.dirname(__file__)
 os.environ['PATH'] = os.environ['PATH'] + ":" + DIR
 
 
-# local testing, remove before CI
+# local testing, remove / comment out before CI
 PROXY='127.0.0.1:8080'
-webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
+POBJ = {
     "httpProxy":PROXY,
     "ftpProxy":PROXY,
     "sslProxy":PROXY,
     "noProxy":[],
     "proxyType":"MANUAL"
 }
+webdriver.DesiredCapabilities.CHROME['proxy'] = POBJ
+webdriver.DesiredCapabilities.FIREFOX['proxy'] = POBJ
 
-
-class DuckDuckGoTestCase(unittest.TestCase):
-
-    def setUp(self):
-        profile = webdriver.FirefoxProfile()
-        profile.add_extension(extension=os.path.join(DIR, "..", "addon"))
-        self.browser = webdriver.Firefox(firefox_profile=profile)
-
+class DuckMixin(object):
     def testDuckLoads(self):
         self.browser.get("https://duckduckgo.com")
         self.assertIn("duckduckgo", self.browser.title.lower())
@@ -41,8 +36,25 @@ class DuckDuckGoTestCase(unittest.TestCase):
         # assert that browser.find_element_by_class_name("js-safe-search-temp") still exists, else throws exception
         self.browser.find_element_by_class_name("js-safe-search-temp")
 
+
+class DuckDuckGoTestCaseFirefox(unittest.TestCase, DuckMixin):
+    def setUp(self):
+        profile = webdriver.FirefoxProfile()
+        profile.add_extension(extension=os.path.join(DIR, "..", "addon"))
+        self.browser = webdriver.Firefox(firefox_profile=profile)
+
     def tearDown(self):
         self.browser.close()
+
+class DuckDuckGoTestCaseChromium(unittest.TestCase, DuckMixin):
+    def setUp(self):
+        profile = webdriver.chrome.options.Options()
+        profile.add_extension(extension=os.path.join(DIR, "..", "safe.zip"))
+        self.browser = webdriver.Chrome(chrome_options=profile)
+
+    def tearDown(self):
+        self.browser.close()
+        
 
 
 if __name__ == "__main__":
