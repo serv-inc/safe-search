c6713c643bed3a6939d573da46791ded6a3a3d7c
serv-inc <serv-inc@users.noreply.github.com>
2019-06-20 15:00:09 +0200

    test: lacked 1st duckduckgo test

--- test/browserstack/conftest.py
+++ test/browserstack/conftest.py
@@ -1,9 +1,24 @@
-import pytest
+import os
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 import secret as s
+
+desired_cap = {
+    'browser': 'Chrome',
+    'browser_version': '62.0',
+    'os': 'Windows',
+    'os_version': '10',
+    'resolution': '1024x768',
+    'name': 'Bstack Tests'
+}
+opt = webdriver.chrome.options.Options()
+opt.add_extension(extension=os.path.join(
+    os.path.dirname(__file__), "..", "..", "safe.zip"))
+desired_cap.update(opt.to_capabilities())
+
 
 @pytest.fixture
 def secret():
@@ -12,8 +27,8 @@ def secret():
 
 @pytest.fixture
 def driver(secret):
-    url= 'http://{}@hub.browserstack.com:80/wd/hub'.format(secret)
+    url = 'http://{}@hub.browserstack.com:80/wd/hub'.format(secret)
     driver = webdriver.Remote(command_executor=url,
-                              desired_capabilities=DesiredCapabilities.FIREFOX)
+                              desired_capabilities=desired_cap)
     yield driver
     driver.quit()
--- test/browserstack/simpleunittest.py
+++ test/browserstack/simpleunittest.py
@@ -1,7 +1,7 @@
 import inspect
 
 import requests
-
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -25,3 +25,16 @@ def fail_browserstack(driver, secret, where):
     "https://api.browserstack.com/automate/sessions/{}.json".format(driver.session_id),
     auth=tuple(secret.split(":")),
     json={"status": "failed", "reason": "test failed in {}".format(where)})  
+
+
+def test_duck(driver, secret):
+  driver.get("https://duckduckgo.com")
+  (driver.find_element_by_id("search_form_input_homepage")
+   .send_keys("porn" + webdriver.common.keys.Keys.RETURN))
+  driver.implicitly_wait(10)
+  try:
+    driver.find_element_by_class_name("js-safe-search-temp").click()
+    driver.find_element_by_class_name("js-safe-search-temp")
+  except selenium.common.exceptions.NoSuchElementException:
+    fail_browserstack(driver, secret, inspect.currentframe().f_code.co_name)
+    assert False, "safe search element does not exist"
