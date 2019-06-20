c0d4c289b081463453a3419ad16eb0f1c192dc1f
serv-inc <serv-inc@users.noreply.github.com>
2019-06-20 14:42:52 +0200

    test: lacked way to tell browserstack that test failed

--- test/browserstack/simpleunittest.py
+++ test/browserstack/simpleunittest.py
@@ -1,15 +1,27 @@
+import inspect
+
+import requests
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-def test_run(secret):
-  url= 'http://{}@hub.browserstack.com:80/wd/hub'.format(secret)
-  driver = webdriver.Remote(command_executor=url, desired_capabilities=DesiredCapabilities.FIREFOX)
+def test_run(driver, secret):
   driver.get('http://www.google.com')
   if not "Google" in driver.title:
+    fail_browserstack(driver, secret, inspect.currentframe().f_code.co_name)    
     raise Exception("Are you not on google? How come!")
   elem = driver.find_element_by_name("q")
   elem.send_keys("selenium")
   elem.submit()
-  driver.quit()
+
+# def test_fail(driver, secret):
+#   fail_browserstack(driver, secret, inspect.currentframe().f_code.co_name)
+#   raise Exception("failed test, should fail")
+
+def fail_browserstack(driver, secret, where):
+  requests.put(
+    "https://api.browserstack.com/automate/sessions/{}.json".format(driver.session_id),
+    auth=tuple(secret.split(":")),
+    json={"status": "failed", "reason": "test failed in {}".format(where)})  
