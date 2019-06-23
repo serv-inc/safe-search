bff0372852e7aeda5c5f671885a9ddd08a6b4d3f
serv-inc <serv-inc@users.noreply.github.com>
2019-06-23 21:59:14 +0200

    test: lacked google browserstack test

--- test/browserstack/simpleunittest.py
+++ test/browserstack/simpleunittest.py
@@ -7,25 +7,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-def test_run(driver, secret):
-  driver.get('http://www.google.com')
-  if not "Google" in driver.title:
-    fail_browserstack(driver, secret, inspect.currentframe().f_code.co_name)    
-    raise Exception("Are you not on google? How come!")
-  elem = driver.find_element_by_name("q")
-  elem.send_keys("selenium")
-  elem.submit()
-
-# def test_fail(driver, secret):
-#   fail_browserstack(driver, secret, inspect.currentframe().f_code.co_name)
-#   raise Exception("failed test, should fail")
-
 def fail_browserstack(driver, secret, where):
   requests.put(
     "https://api.browserstack.com/automate/sessions/{}.json".format(driver.session_id),
     auth=tuple(secret.split(":")),
     json={"status": "failed", "reason": "test failed in {}".format(where)})  
 
+
+def test_google(driver, secret):
+  driver.get('http://images.google.com')
+  (driver
+   .find_element_by_css_selector("input[type=text]")
+   .send_keys("porn" + webdriver.common.keys.Keys.RETURN))
+  driver.implicitly_wait(10)
+  try:
+    driver.find_element_by_id("ss-bimodal-default")
+  except selenium.common.exceptions.NoSuchElementException:
+    fail_browserstack(driver, secret, inspect.currentframe().f_code.co_name)
+    pytest.fail("safe search element does not exist")
 
 def test_duck(driver, secret):
   driver.get("https://duckduckgo.com")
