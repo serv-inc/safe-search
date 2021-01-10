8ddcb0a8870619960b3368da4e7887441164ce86
serv-inc <serv-inc@users.noreply.github.com>
2021-01-10 22:02:25 +0100

    was ugly: lacked eslint

--- addon/background.js
+++ addon/background.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 /* jshint esversion: 6, strict: global, laxbreak: true */
-/* globals chrome, $set, URL, URLSearchParams, window */
+/* globals chrome, $set, URL, URLSearchParams */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
 
@@ -12,84 +12,100 @@
 // =========== URLS ==================
 /** redirects all GET urls to safe search variants */
 chrome.webRequest.onBeforeSendHeaders.addListener(
-  redirectBefore,
-  {urls: ["<all_urls>"], types: ["main_frame", "sub_frame", "xmlhttprequest"]},
-  ["blocking"]
+    redirectBefore,
+    {
+      urls: ['<all_urls>'],
+      types: ['main_frame', 'sub_frame', 'xmlhttprequest'],
+    },
+    ['blocking'],
 );
 
 /** redirects google chrome's omnibox that does not reload the page */
 chrome.webNavigation.onReferenceFragmentUpdated.addListener(function(details) {
   if ( /(webhp|search).*q=/.test(details.url) ) {
-    let new_url = _meta_add(details.url, ["safe", "ssui"], ["active", "on"]);
-    if ( new_url ) {
-      chrome.tabs.update(details.tabId, {'url': new_url});
+    const newUrl = _metaAdd(details.url, ['safe', 'ssui'], ['active', 'on']);
+    if ( newUrl ) {
+      chrome.tabs.update(details.tabId, {'url': newUrl});
     }
   }
-}, {url: [{hostContains: "google"}]});
+}, {url: [{hostContains: 'google'}]});
 
 // DDG otherwise fails
 chrome.webNavigation.onDOMContentLoaded.addListener(
-  redirectDOM,
-  {url: [{hostSuffix: "duckduckgo.com"}]}
+    redirectDOM,
+    {url: [{hostSuffix: 'duckduckgo.com'}]},
 );
 
+/** redirect all relevant URLs to safe search variants
+* @param {Object} requestDetails received from onBeforeSendHeaders
+* @return {Object} value fitting for onBeforeSendHeaders */
 function redirectBefore(requestDetails) {
-  let redir_url = _alter(requestDetails.url);
-  if ( redir_url ) {
-    return { redirectUrl: redir_url };
+  const redirUrl = _alter(requestDetails.url);
+  if ( redirUrl ) {
+    return {redirectUrl: redirUrl};
   }
 }
 
-// needed for DDG as the other callback is not called
+/** alternative redirect, as redirectBefore not called for DDG
+* @param {Object} requestDetails received from onBeforeSendHeaders */
 function redirectDOM(requestDetails) {
-  let redir_url = _alter(requestDetails.url);
-  if ( redir_url ) {
-      chrome.tabs.update(requestDetails.tabId, {'url': redir_url});
+  const redirUrl = _alter(requestDetails.url);
+  if ( redirUrl ) {
+    chrome.tabs.update(requestDetails.tabId, {'url': redirUrl});
   }
 }
 
 
-/** alters url if needs to for safe search, false if no change needed */
+/** Modify uri for safe search
+@param {string} uri the uri to maybe alter
+@return {string} altered url if needed for safe search,
+false if no change needed */
 function _alter(uri) {
   if ( /bing\..*(\/search|\/videos|\/images|\/news)/.test(uri) ) {
-      return _meta_add(uri, ["adlt"], ["strict"]);
+    return _metaAdd(uri, ['adlt'], ['strict']);
   } else if ( /duckduckgo\..*q=/.test(uri) ) {
-      return _meta_add(uri, ["kp"], ["1"]);
+    return _metaAdd(uri, ['kp'], ['1']);
   } else if ( /ecosia.*search/.test(uri) ) {
-    return _meta_add(uri, ["safesearch"], ["2"]);
+    return _metaAdd(uri, ['safesearch'], ['2']);
   } else if ( /google.*(webhp|search).*q=/.test(uri) ) {
-    return _meta_add(uri, ["safe", "ssui"], ["active", "on"]);
-  } else if ( /qwant.com/.test(uri) ) {  // seems to not be heeded
-    return _meta_add(uri, ["safesearch", "s"], ["2", "2"]);
+    return _metaAdd(uri, ['safe', 'ssui'], ['active', 'on']);
+  } else if ( /qwant.com/.test(uri) ) { // seems to not be heeded
+    return _metaAdd(uri, ['safesearch', 's'], ['2', '2']);
   } else if ( /search.yahoo.*\/search/.test(uri) ) {
-    return _meta_add(uri, ["vm"], ["r"]);
+    return _metaAdd(uri, ['vm'], ['r']);
   } else if ( /yandex\..*\/search/.test(uri) ) {
-    return _meta_add(uri, ["fyandex"], ["1"]);
+    return _metaAdd(uri, ['fyandex'], ['1']);
   } else if ( /onesearch.*\/search/.test(uri) ) {
-    return _meta_add(uri, ["vm"], ["r"]);
+    return _metaAdd(uri, ['vm'], ['r']);
   }
   return false;
 }
 
 // qwant does some weird stuff, redirecting failed with CORS on FF
 chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    if (typeof(changeInfo.url) !== "undefined") {
-      if ( /qwant\..*q=/.test(changeInfo.url) ) {
+    function(tabId, changeInfo, tab) {
+      if (typeof(changeInfo.url) !== 'undefined') {
+        if ( /qwant\..*q=/.test(changeInfo.url) ) {
+        }
       }
-    }
-  }
+    },
 );
 
-/** @return uri with key[i]=value[i] for all i instead of or in addition to other params, false if no change needed */
-function _meta_add(uri, keys, values) {
-  let newurl = new URL(uri);
-  let params = new URLSearchParams(newurl.search);
+/**
+@param {str} uri to modify
+@param {Array} keys of URLParam keys
+@param {Array} values of URLParam values
+@return {string } uri with key[i]=value[i]
+* for all i instead of or in addition to other params,
+* false if no change needed */
+function _metaAdd(uri, keys, values) {
+  const newurl = new URL(uri);
+  const params = new URLSearchParams(newurl.search);
   let okcount = 0;
   for (let i = 0; i < keys.length; i++) {
-    if ( params.has(keys[i])
-         && params.get(keys[i]) === values[i]
-         && params.getAll(keys[i]).length === 1 ) {
+    if ( params.has(keys[i]) &&
+         params.get(keys[i]) === values[i] &&
+         params.getAll(keys[i]).length === 1 ) {
       okcount += 1;
     }
   }
@@ -99,7 +115,7 @@ function _meta_add(uri, keys, values) {
   for (let i = 0; i < keys.length; i++) {
     params.set(keys[i], values[i]);
   }
-  newurl.search = "?" + params.toString();
+  newurl.search = '?' + params.toString();
   return newurl.href;
 }
 
@@ -107,59 +123,61 @@ function _meta_add(uri, keys, values) {
 // copied and adjusted from chrome.webRequest docs
 /** adds youtube restricted header to youtube requests */
 chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    if ( $set.youtube > 0 ) {
-      for (let i = 0; i < details.requestHeaders.length; ++i) {
-        if (details.requestHeaders[i].name === 'YouTube-Restrict') {
-          details.requestHeaders.splice(i, 1);
-          break;
+    function(details) {
+      if ( $set.youtube > 0 ) {
+        for (let i = 0; i < details.requestHeaders.length; ++i) {
+          if (details.requestHeaders[i].name === 'YouTube-Restrict') {
+            details.requestHeaders.splice(i, 1);
+            break;
+          }
         }
       }
-    }
-    if ( $set.youtube === 1 ) {
-      details.requestHeaders.push({"name": "YouTube-Restrict",
-                                   "value": "Moderate"});
-    } else if ( $set.youtube === 2 ) {
-      details.requestHeaders.push({"name": "YouTube-Restrict",
-                                   "value": "Strict"});
-    }
-    return {requestHeaders: details.requestHeaders};
-  },
-  {urls: ["*://*.youtube.com/*", "*://clients1.google.com/*client=youtube*"],
-   types: ["main_frame", "sub_frame"]},
-  ["blocking", "requestHeaders"]
+      if ( $set.youtube === 1 ) {
+        details.requestHeaders.push({'name': 'YouTube-Restrict',
+          'value': 'Moderate'});
+      } else if ( $set.youtube === 2 ) {
+        details.requestHeaders.push({'name': 'YouTube-Restrict',
+          'value': 'Strict'});
+      }
+      return {requestHeaders: details.requestHeaders};
+    },
+    {urls: ['*://*.youtube.com/*', '*://clients1.google.com/*client=youtube*'],
+      types: ['main_frame', 'sub_frame']},
+    ['blocking', 'requestHeaders'],
 );
 
 
 // =========== COOKIES ==================
 // todo: document, target specific setting if someone complains
 const DUCKIE = {
-  url: "http://duckduckgo.com/",
-  name: "p",
-  value: "1",
-  domain: "duckduckgo.com",
-  expirationDate: Math.pow(2, 32)-1
+  url: 'http://duckduckgo.com/',
+  name: 'p',
+  value: '1',
+  domain: 'duckduckgo.com',
+  expirationDate: Math.pow(2, 32)-1,
 };
 chrome.cookies.set(DUCKIE);
 
 const VIMMIEMIN = {
-  url: "https://vimeo.com/",
-  name: "content_rating",
-  value: "191",
-  domain: ".vimeo.com",
-  expirationDate: Math.pow(2, 32)-1
+  url: 'https://vimeo.com/',
+  name: 'content_rating',
+  value: '191',
+  domain: '.vimeo.com',
+  expirationDate: Math.pow(2, 32)-1,
 };
-/** @return if cookie is ok: blocks nudity */
+
+/** @param {Object} cookie
+@return {boolean} if cookie is ok: blocks nudity */
 function vimeoOk(cookie) {
   return (cookie !== null) && (255 - cookie.value & 64);
 }
 chrome.cookies.get(
-  {url: "https://vimeo.com", name: "content_rating"},
-  (cookie) => {
-    if (!vimeoOk(cookie)) {
-      chrome.cookies.set(VIMMIEMIN);
-    }
-  }
+    {url: 'https://vimeo.com', name: 'content_rating'},
+    (cookie) => {
+      if (!vimeoOk(cookie)) {
+        chrome.cookies.set(VIMMIEMIN);
+      }
+    },
 );
 
 /** REMOVES COOKIES: main cookie from ixquick/startpage, dogpile's
@@ -170,78 +188,78 @@ chrome.cookies.onChanged.addListener(function(changeInfo) {
     return;
   }
 
-  if ( changeInfo.cookie.domain === ".bing.com" &&
-       changeInfo.cookie.name === "_SS" ) {
+  if ( changeInfo.cookie.domain === '.bing.com' &&
+       changeInfo.cookie.name === '_SS' ) {
     _removeCookie(changeInfo.cookie);
   }
 
-  if ( changeInfo.cookie.domain === ".bing.com" &&
-       changeInfo.cookie.name === "SRCHHPGUSR" ) {
+  if ( changeInfo.cookie.domain === '.bing.com' &&
+       changeInfo.cookie.name === 'SRCHHPGUSR' ) {
     _removeCookie(changeInfo.cookie);
   }
 
-  if ( changeInfo.cookie.domain === ".dailymotion.com" &&
-       changeInfo.cookie.name === "ff" ) {
+  if ( changeInfo.cookie.domain === '.dailymotion.com' &&
+       changeInfo.cookie.name === 'ff' ) {
     _removeCookie(changeInfo.cookie);
   }
 
-  if ( changeInfo.cookie.domain === "www.dogpile.com"
-       && changeInfo.cookie.name === "ws_prefs" ) {
+  if ( changeInfo.cookie.domain === 'www.dogpile.com' &&
+       changeInfo.cookie.name === 'ws_prefs' ) {
     _removeCookie(changeInfo.cookie);
   }
 
-  if ( changeInfo.cookie.domain === "duckduckgo.com"
-       && changeInfo.cookie.name === "p" ) {
+  if ( changeInfo.cookie.domain === 'duckduckgo.com' &&
+       changeInfo.cookie.name === 'p' ) {
     _removeCookie(changeInfo.cookie);
     chrome.cookies.set(DUCKIE);
   }
 
-  if ( changeInfo.cookie.domain === "duckduckgo.com"
-       && changeInfo.cookie.name === "g" ) {
+  if ( changeInfo.cookie.domain === 'duckduckgo.com' &&
+       changeInfo.cookie.name === 'g' ) {
     _removeCookie(changeInfo.cookie);
   }
 
-  if ( changeInfo.cookie.domain === ".ecosia.org"
-       && changeInfo.cookie.name === "ECFG" ) {
+  if ( changeInfo.cookie.domain === '.ecosia.org' &&
+       changeInfo.cookie.name === 'ECFG' ) {
     _removeCookie(changeInfo.cookie);
   }
 
-  if ( changeInfo.cookie.domain === ".patreon.com"
-       && changeInfo.cookie.name === "can_see_nsfw" ) {
+  if ( changeInfo.cookie.domain === '.patreon.com' &&
+       changeInfo.cookie.name === 'can_see_nsfw' ) {
     _removeCookie(changeInfo.cookie);
     chrome.tabs.reload();
   }
-  
-  if ( changeInfo.cookie.domain === ".startpage.com"
-       && changeInfo.cookie.name === "preferences" ) {
+
+  if ( changeInfo.cookie.domain === '.startpage.com' &&
+       changeInfo.cookie.name === 'preferences' ) {
     _removeCookie(changeInfo.cookie);
   }
 
-  if ( changeInfo.cookie.domain === ".reddit.com"
-       && changeInfo.cookie.name === "over18" ) {
+  if ( changeInfo.cookie.domain === '.reddit.com' &&
+       changeInfo.cookie.name === 'over18' ) {
     _removeCookie(changeInfo.cookie);
   }
 
   // also needs to filter request for first request, see below
-  if ( changeInfo.cookie.domain === ".vimeo.com"
-       && changeInfo.cookie.name === "content_rating" ) {
+  if ( changeInfo.cookie.domain === '.vimeo.com' &&
+       changeInfo.cookie.name === 'content_rating' ) {
     if (!vimeoOk(changeInfo.cookie)) {
       _removeCookie(changeInfo.cookie);
       chrome.cookies.set(VIMMIEMIN);
     }
   }
 
-  if ( changeInfo.cookie.domain === ".youtube.com" ) {
-    if ( changeInfo.cookie.name === "dkv" ) {
+  if ( changeInfo.cookie.domain === '.youtube.com' ) {
+    if ( changeInfo.cookie.name === 'dkv' ) {
       _removeCookie(changeInfo.cookie);
-    } else if ( changeInfo.cookie.name === "PREFS" && $set.youtube >= 1 ) {
-      let prefs = new URLSearchParams(changeInfo.cookie.value);
-      if ( prefs.get("f2") !== "8000000" ) {
-        prefs.set("f2", "8000000");
+    } else if ( changeInfo.cookie.name === 'PREFS' && $set.youtube >= 1 ) {
+      const prefs = new URLSearchParams(changeInfo.cookie.value);
+      if ( prefs.get('f2') !== '8000000' ) {
+        prefs.set('f2', '8000000');
         chrome.cookies.set({
-          "url": "https://youtube.com",
-          "name": "PREFS",
-          "value": prefs.toString()
+          'url': 'https://youtube.com',
+          'name': 'PREFS',
+          'value': prefs.toString(),
         });
       }
     }
@@ -249,8 +267,9 @@ chrome.cookies.onChanged.addListener(function(changeInfo) {
 });
 
 // courtesy of cookie api test extension, as of stackoverflow
+/** @param {Object} cookie to remove */
 function _removeCookie(cookie) {
-  let url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain +
+  const url = 'http' + (cookie.secure ? 's' : '') + '://' + cookie.domain +
       cookie.path;
-  chrome.cookies.remove({"url": url, "name": cookie.name});
+  chrome.cookies.remove({'url': url, 'name': cookie.name});
 }
--- addon/settings.js
+++ addon/settings.js
@@ -1,6 +1,7 @@
-"use strict";
+'use strict';
 /* jshint esversion: 6, strict: global, loopfunc: true, laxbreak: true */
-/* globals chrome, console, XMLHttpRequest, setTimeout */
+/* globals chrome, XMLHttpRequest */
+/* exported getSettings */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
 /**
@@ -9,26 +10,27 @@
 class Settings {
   /** initializes from managed, local storage. on first load from preset.json */
   constructor() {
-    let _self = this;
+    const _self = this;
     this._initialized = false;
     this._listeners = {};
     this._loaded = false;
     this._managed = [];
     this._saved = true;
     this._settings = {};
-    let managedPolyfill = (chrome.storage.managed || { get: (a, b) => b({}) });
-    managedPolyfill.get(null, result => {
-      if ( typeof(result) !== "undefined" ) {
-        for (let el in result) {
+    const managedPolyfill = (chrome.storage.managed || {get: (a, b) => b({})});
+    managedPolyfill.get(null, (result) => {
+      if ( typeof(result) !== 'undefined' ) {
+        for (const el in result) {
           if ( result.hasOwnProperty(el) ) {
             this._managed.push(el);
             this._addToSettings(el, result[el]);
           }
         }
       }
-      chrome.storage.local.get(null, result => {
-        for (let el in result) {
-          if ( el === "_initialized" ) {
+      chrome.storage.local.get(null, (result) => {
+        // eslint-disable-next-line guard-for-in
+        for (const el in result) {
+          if ( el === '_initialized' ) {
             this._initialized = true;
             continue;
           }
@@ -45,100 +47,110 @@ class Settings {
     });
     // if a managed option becomes unmanaged, use the managed setting as local
     chrome.storage.onChanged.addListener((changes, area) => {
-      for (let el in changes) {
+      for (const el in changes) {
         if ( changes.hasOwnProperty(el) ) {
-          if ( area === "managed" ) {
+          if ( area === 'managed' ) {
             if ( ! _self.isManaged(el) ) { // create
               _self._managed.push(el);
             } else { // update or delete
-              if ( ! changes[el].hasOwnProperty('newValue') ) { // got deleted, use as local
+              // got deleted, use as local
+              if ( ! changes[el].hasOwnProperty('newValue') ) {
                 _self._managed.splice(_self._managed.indexOf(el));
               }
             }
           }
           _self._addToSettings(el, changes[el].newValue);
           if ( el in _self._listeners ) {
-            _self._listeners[el].forEach(el => el(changes, area));
+            _self._listeners[el].forEach((el) => el(changes, area));
           }
         }
       }
     });
   }
 
-
+  /** @return {RegExp} of whitelist */
   get whitelistRegExp() {
     return RegExp(this.whitelist);
   }
 
-
-  _addToSettings(el, val) {
-    this._settings[el] = val;
-    this._addGetSet(el, !this.isManaged(el));
+  /** @param {string} key to add
+      @param {string} value for key */
+  _addToSettings(key, value) {
+    this._settings[key] = value;
+    this._addGetSet(key, !this.isManaged(key));
   }
 
 
   // could also trigger a save of that value via set
-  _addGetSet(el, setter=false) {
+  /** @param {string} key to add getter and setter for
+      @param {boolean} setter true if should add setter, else only getter */
+  _addGetSet(key, setter=false) {
     if ( setter ) {
-      Object.defineProperty(this, el,
-                            { get: () => { return this._settings[el]; },
-                              set: (x) => {
-                                this._settings[el] = x;
-                                this._saved = false;
-                              },
-                              configurable: true });
+      Object.defineProperty(this, key,
+          {get: () => {
+            return this._settings[key];
+          },
+          set: (x) => {
+            this._settings[key] = x;
+            this._saved = false;
+          },
+          configurable: true});
     } else {
-      Object.defineProperty(this, el,
-                            { get: () => { return this._settings[el]; },
-                              configurable: true });
+      Object.defineProperty(this, key,
+          {get: () => {
+            return this._settings[key];
+          },
+          configurable: true});
     }
   }
 
-
-  addOnChangedListener(val, listener) {
-    if ( val in this._listeners ) {
-      this._listeners[val].push(listener);
+  /** @param {string} key
+      @param {Object} listener function to call when key's value changes */
+  addOnChangedListener(key, listener) {
+    if ( key in this._listeners ) {
+      this._listeners[key].push(listener);
     } else {
-      this._listeners[val] = [listener];
+      this._listeners[key] = [listener];
     }
   }
 
-
+  /** loading done, save status */
   finish() {
     this._loaded = true;
     this.save();
   }
 
-
-  isManaged(el) {
-    return this._managed.includes(el);
+  /** @param {string} key to check if managed
+      @return {boolean} true if key is in managed storage */
+  isManaged(key) {
+    return this._managed.includes(key);
   }
 
-
+  /** save to local storage */
   save() {
-    let out = {"_initialized": true};
-    for (let el in this._settings) {
-      if ( ! this.isManaged(el) && typeof(el) !== "undefined") {
+    const out = {'_initialized': true};
+    for (const el in this._settings) {
+      if ( ! this.isManaged(el) && typeof(el) !== 'undefined') {
         out[el] = this._settings[el];
       }
     }
-    if (typeof(chrome.storage) !== "undefined") {
+    if (typeof(chrome.storage) !== 'undefined') {
       chrome.storage.local.set(out);
       this._saved = true;
     }
   }
 
-
+  /** load settings from file */
   _loadFileSettings() {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
+    const xobj = new XMLHttpRequest();
+    xobj.overrideMimeType('application/json');
     xobj.open('GET', 'preset.json', true);
     xobj.onreadystatechange = () => {
-      if (xobj.readyState == 4 && xobj.status == "200") {
-        let parsed = JSON.parse(xobj.responseText);
-        for (let el in parsed) {
-          if ( parsed.hasOwnProperty(el)
-               && ! this.isManaged(el) ) {
+      if (xobj.readyState == 4 && xobj.status == '200') {
+        const parsed = JSON.parse(xobj.responseText);
+        for (const el in parsed) {
+          if ( parsed.hasOwnProperty(el) &&
+               ! this.isManaged(el) ) {
             this._addToSettings(el, parsed[el]);
           }
         }
@@ -149,6 +161,10 @@ class Settings {
   }
 }
 
-let $set = new Settings();
-/** @return settings to options page */
-function getSettings() { return $set; }
+const $set = new Settings();
+
+// eslint-disable no-unused-vars
+/** @return {Object} settings to options page */
+function getSettings() {
+  return $set;
+}
