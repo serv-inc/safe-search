a8b8ecdbd44c0495065017b891af0c5fbdc2a282
serv-inc <serv-inc@users.noreply.github.com>
2022-01-22 12:52:12 +0100

    bug: did not work

--- addon/google_safe_fallback.js
+++ addon/google_safe_fallback.js
@@ -1,5 +1,5 @@
 /** @fileinfo clicks safe search button as a fallback */
 const script = document.createElement("script");
-script.textContent = ```document.querySelector('a[href*="safeui=on"')?.click();```;
+script.textContent = `document.querySelector('a[href*="safeui=on"')?.click();`;
 (document.head || document.documentElement).appendChild(script);
 script.remove();
