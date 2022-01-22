0cda88e2b2a297f93de1cbc79718d7e24306f546
serv-inc <serv-inc@users.noreply.github.com>
2022-01-22 12:59:22 +0100

    lacked test: eslint moz plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:no-unsanitized/DOM"],
   parserOptions: {
     ecmaVersion: 12,
   },
