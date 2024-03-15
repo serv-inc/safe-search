d82cb7e58be845af0cba04a69cfbc95c7e58bfbc
Pierre <8363334+trollepierre@users.noreply.github.com>
2024-03-15 11:27:15 +0100

    Update qwant_safe.js
    remove comment

--- addon/qwant_safe.js
+++ addon/qwant_safe.js
@@ -1,18 +1,16 @@
 function setSafe() {
-  var cookies = document.cookie.split(";"); // Récupérer tous les cookies existants
+  var cookies = document.cookie.split(";");
   var updatedCookie = false;
 
   for (var i = 0; i < cookies.length; i++) {
     var cookie = cookies[i].trim();
     if (cookie.startsWith("s=")) {
-      // Mettre à jour le cookie "s" en remplaçant sa valeur par "2"
       document.cookie = "s=2; " + cookie.substring(2);
       updatedCookie = true;
       break;
     }
   }
 
-  // Si le cookie "s" n'existe pas, le créer avec la valeur "2"
   if (!updatedCookie) {
     document.cookie = "s=2";
   }
