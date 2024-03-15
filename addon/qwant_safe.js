6e23ec4d981f8592ca4c4d4cc07e2aa826e0b7b6
Pierre <8363334+trollepierre@users.noreply.github.com>
2024-03-15 11:24:18 +0100

    fix(qwant): safe search is now using cookie at qwant

--- addon/qwant_safe.js
+++ addon/qwant_safe.js
@@ -1,11 +1,21 @@
-/** @fileinfo clicks safe search button as a fallback */
 function setSafe() {
-  const script = document.createElement("script");
-  script.textContent = `applicationState.user.userSetting.safeSearch = 2;
-delete siteConfiguration.safeSearchModes[0];
-delete siteConfiguration.safeSearchModes[1];`;
-  (document.head || document.documentElement).appendChild(script);
-  script.remove();
+  var cookies = document.cookie.split(";"); // Récupérer tous les cookies existants
+  var updatedCookie = false;
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.startsWith("s=")) {
+      // Mettre à jour le cookie "s" en remplaçant sa valeur par "2"
+      document.cookie = "s=2; " + cookie.substring(2);
+      updatedCookie = true;
+      break;
+    }
+  }
+
+  // Si le cookie "s" n'existe pas, le créer avec la valeur "2"
+  if (!updatedCookie) {
+    document.cookie = "s=2";
+  }
 }
 
 setSafe();
